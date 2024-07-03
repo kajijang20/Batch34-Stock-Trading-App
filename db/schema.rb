ea22c21b382297e16e77fb8cd1747c09d8dbc9f2
kajijang20 <kajijangching@gmail.com>
2024-07-03 19:17:11 +0800

    removed similar name order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_102300) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_130209) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,11 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_29_102300) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
