8a1936ff73c3f0ce49f5ea1de4712b926e201ffe
Anst09 <anp.obras97@gmail.com>
2024-07-14 20:24:53 +0800

    dropped stock table

--- db/migrate/20240621125756_devise_create_users.rb
+++ db/migrate/20240621125756_devise_create_users.rb
@@ -34,6 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
 
       t.integer :status
       t.timestamps null: false
+      
     end
 
     add_index :users, :email,                unique: true
