1e1739ce3165a0efd661839e7195d2b084e63a11
Anst09 <anp.obras97@gmail.com>
2024-07-14 20:24:53 +0800

    dropped stock table

--- db/migrate/20240701101020_create_portfolios.rb
+++
@@ -1,8 +0,0 @@
-class CreatePortfolios < ActiveRecord::Migration[7.1]
-  def change
-    create_table :portfolios do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20240701101338_create_trader_stocks.rb
+++
@@ -1,8 +0,0 @@
-class CreateTraderStocks < ActiveRecord::Migration[7.1]
-  def change
-    create_table :trader_stocks do |t|
-
-      t.timestamps
-    end
-  end
-end
