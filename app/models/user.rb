d1a6138763d5eb31a805991efdda258f7763ddb5
Anst09 <anp.obras97@gmail.com>
2024-07-16 00:48:07 +0800

    reconfigure controllers, modified order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,30 @@
 class User < ApplicationRecord
   after_create :send_welcome_email
+  before_create :initialize_wallet
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :trader_stocks
   has_many :orders
   rolify
 
   def has_role?(role)
-    self.roles.any? { |r| r.name.underscore.to_sym == role }
+    roles.any? { |r| r.name.underscore.to_sym == role }
   end
 
-  enum :status, { pending: 0, approved: 1 }, default: :pending
+  enum status: { pending: 0, approved: 1 }, _default: :pending
 
-  def recalculate_balance price
-    # if order_type == "BUY"
-    update(balance: balance + price)
+  # Initialize wallet with $50,000,000 on user creation
+  def initialize_wallet
+    self.wallet = 50000000
+  end
+
+  # Recalculate the wallet balance
+  def recalculate_balance(amount)
+    update(wallet: wallet + amount)
   end
 
   private
@@ -26,7 +33,4 @@ class User < ApplicationRecord
     # You can replace this with your actual implementation to send the email
     UserMailer.welcome_email(self).deliver_now
   end
-
 end
-
-
